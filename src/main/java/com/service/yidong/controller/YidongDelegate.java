b9c3c2c21614bfda91257ebe0d96ba0a6f040c44
paas <paas@cse-codegen-backend-965770143-m4qr4>
2018-09-05 09:26:32 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/yidong/controller/YidongDelegate.java
@@ -0,0 +1,14 @@
+package com.service.yidong.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class YidongDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
